8ace8d21e15e92948a2816edba5fd62e4c43e644
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2023-02-27 20:06:51 -0500

    Add files via upload

---
+++ question2.sql
@@ -0,0 +1,65 @@
+begin transaction;
+DROP SCHEMA IF EXISTS AccidInfo CASCADE;
+create schema AccidInfo;
+set search_path to AccidInfo;
+
+create table Personne (
+  codePers text not null,
+  nom text not null,  
+  prenom text not null,   
+  adress text, 
+  age integer not null,
+  primary key (codePers),
+  unique(nom)
+);
+
+create table Vehicule (
+  codeVeh text not null,
+  marque text not null,   
+  typeV text not null, 
+  primary key (codeVeh)
+);
+
+create table Conducteur (
+  codePers text,
+  codeVeh text,   
+  nbacc integer not null, 
+  primary key (codePers),
+  foreign key (codePers) references Personne(codePers) ,
+  foreign key (codeVeh) references Vehicule(codeVeh)
+);
+
+create table Accident (
+  codeAcc text not null,
+  date timestamp,   
+  dept text not null, 
+  primary key (codeAcc)
+);
+
+create table VEHPART (
+  codeAcc text,
+  codeVeh text,   
+  nCond text not null, 
+  primary key (codeAcc, codeVeh),
+  foreign key (codeAcc) references Accident(codeAcc) ,
+  foreign key (codeVeh) references Vehicule(codeVeh)
+);
+
+create table Blesse (
+  codeAcc text,
+  codePers text,
+  codeVeh text,
+  gravite text not null, 
+  primary key (codeAcc, codePers),
+  foreign key (codeAcc) references Accident(codeAcc) ,
+  foreign key (codePers) references Personne(codePers) ,	
+  foreign key (codeVeh) references Vehicule(codeVeh)
+);
+
+
+commit;
+
+
+
+
+
