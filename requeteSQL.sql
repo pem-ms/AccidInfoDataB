8e22ca300eb5f9fe50417cd62ca7ee07f0a03185
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2023-03-01 14:13:54 -0500

    q3

--- requeteSQL.sql
+++ requeteSQL.sql
@@ -2,3 +2,8 @@ WITH r0 as (with r1 as (WITH r2 as( SELECT * FROM vehicule NATURAL JOIN blesse)
 SELECT * FROM r2 WHERE marque = 'BMW' and gravite='Fatale') 
 SELECT * FROM accident NATURAL JOIN r1 WHERE dept = '75' ) 
 SELECT nom, prenom, date FROM personne NATURAL JOIN r0;
+
+
+WITH r1 as (WITH r2 as( SELECT * FROM blesse NATURAL JOIN accident) 
+SELECT * FROM r2 WHERE date = '2023-02-14' and gravite='Grave') 
+SELECT nom, prenom, age from personne NATURAL JOIN r1;
