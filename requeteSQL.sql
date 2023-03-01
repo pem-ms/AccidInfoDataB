405f82e6fbf8f134614a60e80be28e4eeeadecd2
Kanty Gakima <91140943+gakkal29@users.noreply.github.com>
2023-03-01 14:37:26 -0500

    Update requeteSQL.sql

--- requeteSQL.sql
+++ requeteSQL.sql
@@ -4,9 +4,8 @@ SELECT * FROM r2 WHERE marque = 'BMW' and gravite='Fatale')
 SELECT * FROM accident NATURAL JOIN r1 WHERE dept = '75' ) 
 SELECT nom, prenom, date FROM personne NATURAL JOIN r0;
 --2
-WITH r0 as (with r1 as( SELECT codeAcc FROM AccidInfo.VehPart)
-SELECT * FROM AccidInfo.Blesse NATURAL JOIN r1)
-SELECT * FROM AccidInfo.personne NATURAL JOIN r0;
+WITH r0 as (with r1 as(SELECT * FROM AccidInfo.Blesse NATURAL JOIN AccidInfo.VehPart)
+SELECT codePers FROM r1)SELECT nom, prenom,adress,age FROM r0 NATURAL JOIN AccidInfo.Personne;
 --3
 WITH r1 as (WITH r2 as( SELECT * FROM blesse NATURAL JOIN accident) 
 SELECT * FROM r2 WHERE date = '2023-02-14' and gravite='Grave') 
