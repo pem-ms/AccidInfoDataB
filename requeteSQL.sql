43fc900d0e10690787117cd55ae2c0eb74b96714
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2023-03-01 22:35:51 -0500

    Update requeteSQL.sql

--- requeteSQL.sql
+++ requeteSQL.sql
@@ -11,3 +11,8 @@ WITH r1 as (WITH r2 as( SELECT * FROM blesse NATURAL JOIN accident)
 SELECT * FROM r2 WHERE date = '2023-02-14' and gravite='Grave') 
 SELECT nom, prenom, age from personne NATURAL JOIN r1;
 --4
+with r0 as (with r1 as (  WITH r2 as( 
+  SELECT * FROM blesse NATURAL JOIN accident) 
+  SELECT * FROM r2 WHERE date::text LIKE '2022%' AND gravite='Fatale') 
+  SELECT * FROM vehicule NATURAL JOIN r1 WHERE marque = 'Volkswagen') 
+  SELECT COUNT(dept) AS nbofaccident FROM r0 GROUP BY dept;
