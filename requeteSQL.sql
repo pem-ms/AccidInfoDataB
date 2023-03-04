30c3ef05e1a8c4fd375b7e1dafa69d6d5fd7d4e6
Marianne_P <91166970+pem-ms@users.noreply.github.com>
2023-03-04 15:44:58 -0500

    Update requeteSQL.sql

--- requeteSQL.sql
+++ requeteSQL.sql
@@ -1,19 +1,19 @@
 --1
-WITH r0 as (with r1 as (WITH r2 as( SELECT * FROM vehicule NATURAL JOIN blesse) 
+WITH r0 as (with r1 as (WITH r2 as( SELECT * FROM Vehicule NATURAL JOIN Blesse) 
 SELECT * FROM r2 WHERE marque = 'BMW' and gravite='Fatale') 
-SELECT * FROM accident NATURAL JOIN r1 WHERE dept = '75' ) 
-SELECT nom, prenom, date FROM personne NATURAL JOIN r0;
+SELECT * FROM Accident NATURAL JOIN r1 WHERE dept = '75' ) 
+SELECT nom, prenom, date FROM Personne NATURAL JOIN r0;
 --2
 WITH r0 as (with r1 as(SELECT * FROM Blesse NATURAL JOIN VehPart)
 SELECT codePers FROM r1)SELECT nom, prenom,adress,age FROM r0 NATURAL JOIN Personne;
 --3
-WITH r1 as (WITH r2 as( SELECT * FROM blesse NATURAL JOIN accident) 
+WITH r1 as (WITH r2 as( SELECT * FROM Blesse NATURAL JOIN Accident) 
 SELECT * FROM r2 WHERE date = '2023-02-14' and gravite='Grave') 
-SELECT nom, prenom, age from personne NATURAL JOIN r1;
+SELECT nom, prenom, age from Personne NATURAL JOIN r1;
 --4
 with r as ( with r0 as (with r1 as (  WITH r2 as( 
-SELECT * FROM blesse NATURAL JOIN accident) 
+SELECT * FROM Blesse NATURAL JOIN Accident) 
 SELECT * FROM r2 WHERE date::text LIKE '2022%' and gravite = 'Fatale') 
-SELECT * FROM vehicule NATURAL JOIN r1 WHERE marque = 'Volkswagen') 
-SELECT * FROM conducteur NATURAL JOIN r0) 
+SELECT * FROM Vehicule NATURAL JOIN r1 WHERE marque = 'Volkswagen') 
+SELECT * FROM Conducteur NATURAL JOIN r0) 
 SELECT sum(nbacc) as nbaccident, dept FROM r GROUP BY dept;
