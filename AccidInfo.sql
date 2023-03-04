5c60340c6461ce637418403fd6064fe47e5a729a
Marianne_P <91166970+pem-ms@users.noreply.github.com>
2023-03-04 15:12:57 -0500

    Update AccidInfo.sql

--- AccidInfo.sql
+++ AccidInfo.sql
@@ -4,47 +4,46 @@ create schema AccidInfo;
 set search_path to AccidInfo;
 
 create table Personne (
-  codePers serial,
+  codePers serial not null,
   nom text not null,  
   prenom text not null,   
   adress text not null, 
   age integer CHECK(age>=18),
-  primary key (codePers),
-  unique(nom)
+  primary key (codePers)
 );
 
 create table Vehicule (
-  codeVeh integer,
+  codeVeh integer not null,
   marque text not null,   
   typeV text, 
   primary key (codeVeh)
 );
 
 create table Conducteur (
-  codePers serial,
-  codeVeh integer,   
+  codePers serial not null,
+  codeVeh integer not null,   
   nbacc integer,
   primary key (codePers, codeVeh)
 );
 
 create table Accident (
-  codeAcc integer,
+  codeAcc integer not null,
   date timestamp,   
   dept text not null CHECK(CAST (dept AS integer) BETWEEN 1 AND 80 ), 
   primary key (codeAcc)
 );
 
 create table VEHPART (
-  codeAcc integer,
-  codeVeh integer,   
+  codeAcc integer not null,
+  codeVeh integer not null,   
   nCond integer,
   primary key (codeAcc, codeVeh)
 );
 
 create table Blesse (
-  codeAcc integer,
-  codePers serial,
-  codeVeh integer,
+  codeAcc integer not null,
+  codePers serial not null,
+  codeVeh integer not null,
   gravite text,
   primary key (codeAcc, codePers)
 );
